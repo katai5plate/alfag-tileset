798be95632bc882f8ea50d1887cb517334c0abe2
katai5plate <katai5plate.sub@gmail.com>
2021-05-30 11:40:35 +0900

    add dropper

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, Typography } from "@material-ui/core";
+import { DropzoneAreaBase, FileObject } from "material-ui-dropzone";
+import { FC, useState, useMemo, Fragment } from "react";
+import bmp from "bmp-js";
 
-function App() {
+const Dropper: FC<{
+  files: FileObject[];
+  setFiles: (fileObject: FileObject[]) => void;
+}> = ({ files, setFiles }) => {
+  const [bitmap, setBitmap] = useState<bmp.BmpDecoder | null>(null);
+  files?.[0]?.file.arrayBuffer().then((x: ArrayBuffer) => {
+    setBitmap(bmp.decode(Buffer.from(x)));
+  });
+  const isDropped = useMemo(() => files.length !== 0, [files]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div style={isDropped ? { display: "none" } : {}}>
+        <DropzoneAreaBase fileObjects={files} onAdd={setFiles} />
+      </div>
+      <div style={!isDropped ? { display: "none" } : {}}>
+        <pre>
+          {bitmap === null
+            ? "Loading..."
+            : JSON.stringify(
+                (() => {
+                  const { buffer, data, ...rest } = bitmap as any;
+                  return rest;
+                })(),
+                null,
+                2
+              )}
+        </pre>
+      </div>
+    </Container>
   );
-}
+};
 
-export default App;
+export default () => {
+  const [files, setFiles] = useState<FileObject[]>([]);
+  return (
+    <Fragment>
+      <Container>
+        <Typography variant="h1">BMP ファイルを D&D</Typography>
+        <Dropper files={files} setFiles={setFiles} />
+      </Container>
+    </Fragment>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import theme from "./theme";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ThemeProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
